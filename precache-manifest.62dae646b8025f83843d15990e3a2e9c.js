f84ee7fabbfeb001cfb9f9237255b2940fa09ba8
Pranav Patel <42827820+PranavPatel292@users.noreply.github.com>
2020-08-27 09:53:46 +1000

    Updates

---
+++ precache-manifest.62dae646b8025f83843d15990e3a2e9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1d8871565be9cb72611218ad0cd7cf",
+    "url": "/AI_Australia/index.html"
+  },
+  {
+    "revision": "db817f37173e38f17905",
+    "url": "/AI_Australia/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e281080ab777c8976432",
+    "url": "/AI_Australia/static/css/main.2076ff03.chunk.css"
+  },
+  {
+    "revision": "db817f37173e38f17905",
+    "url": "/AI_Australia/static/js/2.4b7b5cbe.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/AI_Australia/static/js/2.4b7b5cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e281080ab777c8976432",
+    "url": "/AI_Australia/static/js/main.19fb372e.chunk.js"
+  },
+  {
+    "revision": "179627c6d99bcfe9806f",
+    "url": "/AI_Australia/static/js/runtime-main.e1d9118d.js"
+  },
+  {
+    "revision": "bab14c7e7c55a09b1e4b30a2e59d9b50",
+    "url": "/AI_Australia/static/media/iStock-1026914886.bab14c7e.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AI_Australia/precache-manifest.62dae646b8025f83843d15990e3a2e9c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AI_Australia/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
